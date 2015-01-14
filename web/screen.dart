3b36290cc80b953ea21a2834b001bf1b45ce4638
Andrew O'Neill <foolusion@gmail.com>
2015-01-14 13:27:05 -0500

    fillText isn't working correctly

--- web/screen.dart
+++ web/screen.dart
@@ -40,10 +40,6 @@ class Screen {
     context.save();
     context.translate(blockSize * (w) + -.5, -.5);
     context.strokeRect(0, 0, blockSize * 4, blockSize * 4);
-    context.font = '12pt Roboto';
-    var t = context.measureText('Next');
-    context.fillStyle = 'white';
-    context.fillText('Next', 2, 2 + blockSize * 4 + t.actualBoundingBoxAscent);
     context.restore();
   }
 }
